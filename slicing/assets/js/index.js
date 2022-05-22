0227e47d2e3552a606cecda6074b7dca6f4b31cc
Fadhil <fadhilmhmmd@gmail.com>
2022-05-22 09:09:53 +0700

    add slicing

---
+++ slicing/assets/js/index.js
@@ -0,0 +1,129 @@
+let reviewSlideIndex = 1;
+reviewShowSlides(reviewSlideIndex);
+
+let trendingSlideIndex = 1;
+trendingShowSlide(trendingSlideIndex);
+
+/* ----- Review ----- */
+
+function reviewPlusSlide(n) {
+  reviewShowSlides((reviewSlideIndex += n));
+}
+
+function reviewCurrentSlide(n) {
+  reviewShowSlides((reviewSlideIndex = n));
+}
+
+function reviewShowSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("review-slide");
+  let dots = document.getElementsByClassName("review-carousel-indicator");
+  let leftArrow = document.getElementById(
+    "review-carousel-control__button--left"
+  );
+  let rightArrow = document.getElementById(
+    "review-carousel-control__button--right"
+  );
+
+  if (n > slides.length) {
+    reviewSlideIndex = 1;
+  }
+  if (n < 1) {
+    reviewSlideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  if (reviewSlideIndex !== 1 && !leftArrow.className.includes("active")) {
+    leftArrow.className += " active";
+    leftArrow.setAttribute("onclick", "reviewPlusSlide(-1)");
+  }
+
+  if (reviewSlideIndex === 1) {
+    leftArrow.classList.remove("active");
+    leftArrow.removeAttribute("onclick");
+  }
+
+  if (
+    reviewSlideIndex !== slides.length &&
+    !rightArrow.className.includes("active")
+  ) {
+    rightArrow.className += " active";
+    rightArrow.setAttribute("onclick", "reviewPlusSlide(1)");
+  }
+
+  if (reviewSlideIndex === slides.length) {
+    rightArrow.classList.remove("active");
+    rightArrow.removeAttribute("onclick");
+  }
+
+  slides[reviewSlideIndex - 1].style.display = "flex";
+  dots[reviewSlideIndex - 1].className += " active";
+}
+
+/* ----- Trending ----- */
+
+function trendingPlusSlides(n) {
+  trendingShowSlide((trendingSlideIndex += n));
+}
+
+function trendingCurrentSlide(n) {
+  trendingShowSlide((trendingSlideIndex = n));
+}
+
+function trendingShowSlide(n) {
+  console.log("trendingSlideIndex", trendingSlideIndex);
+  let i;
+  let slides = document.getElementsByClassName("trending-slide");
+  let dots = document.getElementsByClassName("trending-carousel-indicator");
+  let leftArrow = document.getElementById(
+    "trending-carousel-control__button--left"
+  );
+  let rightArrow = document.getElementById(
+    "trending-carousel-control__button--right"
+  );
+
+  if (n > slides.length) {
+    trendingSlideIndex = 1;
+  }
+  if (n < 1) {
+    trendingSlideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    console.log(dots[i].className, "line100");
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  if (trendingSlideIndex !== 1 && !leftArrow.className.includes("active")) {
+    leftArrow.className += " active";
+    leftArrow.setAttribute("onclick", "trendingPlusSlides(-1)");
+  }
+
+  if (trendingSlideIndex === 1) {
+    leftArrow.classList.remove("active");
+    leftArrow.removeAttribute("onclick");
+  }
+
+  if (
+    trendingSlideIndex !== slides.length &&
+    !rightArrow.className.includes("active")
+  ) {
+    rightArrow.className += " active";
+    rightArrow.setAttribute("onclick", "trendingPlusSlides(1)");
+  }
+
+  if (trendingSlideIndex === slides.length) {
+    rightArrow.classList.remove("active");
+    rightArrow.removeAttribute("onclick");
+  }
+
+  slides[trendingSlideIndex - 1].style.display = "flex";
+  dots[trendingSlideIndex - 1].className += " active";
+}
